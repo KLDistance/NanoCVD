e6606f4d11e2d3efc4156f872a5c3ff00201eb55
KLDistance <dripysumer@gmail.com>
2020-07-17 15:38:28 -0400

    change ver number.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowTitle("NanoCVD v0.7");
+    w.setWindowTitle("NanoCVD v0.8");
     w.show();
     return a.exec();
 }
